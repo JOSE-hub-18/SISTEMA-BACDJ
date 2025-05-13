2396f2215073cfaa89d9bd7698dce21bf9c4370d
JOSE-hub-18 <al222550@alumnos.uacj.mx>
2025-05-13 13:22:32 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const archivos = {
     { nombre: "Pre-ID", link: "https://docs.google.com/spreadsheets/d/1OqqRk83uWQLi8VKQCfrgAnUa_Cv89d9ovD86uqQYtUw/edit?usp=drive_link"},
     { nombre: "Pre-Registro Para Primera Vez(formulario)", link: "https://docs.google.com/forms/d/1189K2ODdCtrim0TxB01y8iZOYKli9IxidqUVcP8LeDg/edit" },
     { nombre: "Pre-Registro Para Primera Vez(respuestas)", link: "https://docs.google.com/spreadsheets/d/1d3nJMLffcF8COpMxTtIN0RWlEkXgarvW_0x0GZaRc0M/edit?usp=drive_link" },
-    {nombre: "BACDJ-TS006-A Reglamento y carta compromiso 2025", link: "file:///C:/Users/josel/AppData/Local/Temp/b734f82c-4a4b-4413-a3df-50c289fadd19_DOCUMENTOS_EDITADOS_PDF[1].zip.d19/BACDJ-TS006-A%20%20Reglamento%20y%20carta%20compromiso%202025.docx.pdf" },
+    {nombre: "BACDJ-TS006-A Reglamento y carta compromiso 2025", link: "https://drive.google.com/file/d/1nLNxXqW2Y3d3_-QT89vQApL-TD-vHNBu/view?usp=drive_link" },
     { nombre: "WhatsApp ELY OCHOA", link: "https://docs.google.com/spreadsheets/d/1Bh_lOSJ2aC0S2mvRTfEY2X_ulZCKubrl/edit?usp=drive_link&ouid=105011259160866655546&rtpof=true&sd=true"},
     { nombre: "REGISTROS PRIMERA VEZ 2025", link: "https://docs.google.com/spreadsheets/d/1gFD2Yq3EJMhUMeu_-8AGKdGeDdQrhFJ_7NXjPuzqQmA/edit?usp=drive_link"},
 
@@ -20,7 +20,7 @@ const archivos = {
       { nombre: "INSTITUCIONES/COMEDORES/RELIGIOSAS", link: "https://docs.google.com/spreadsheets/d/1N89_ioh1SHMZsFq7ec4D0rY21Qy_h6GbAoF534DDJzY/edit?usp=drive_link" },
       { nombre: "INSTITUCIONES/ALBERGUES", link: "https://docs.google.com/spreadsheets/d/133rhGFC8p3Zu8OM6R7EeOrxy2_Eti0nr3t349qTKXyo/edit?usp=drive_link"},
       { nombre: "Actualizacion de Comites", link: "https://docs.google.com/spreadsheets/d/1197rpkD8sMRYRb9Xq-IzcdlPeBEwuOyM/edit?usp=drive_link&ouid=105011259160866655546&rtpof=true&sd=true"},
-      {nombre: "BACDJ-TS002-A Producto a granel", link: "file:///C:/Users/josel/AppData/Local/Temp/bf778271-fe04-45b6-95b7-2b5727fbef07_DOCUMENTOS_EDITADOS_PDF[1].zip.f07/BACDJ-TS002-A%20%20Producto%20a%20granel.docx.pdf" },
+      {nombre: "BACDJ-TS002-A Producto a granel", link: "https://drive.google.com/file/d/1ay0_XihQ6fR-vLOt08xvd1hb1H9hdg8y/view?usp=drive_link" },
 
     ],
     atencion: [
@@ -28,22 +28,22 @@ const archivos = {
       { nombre: "Entrega BACDJ A Comite ACTUALIZADO", link: "https://docs.google.com/spreadsheets/d/12lWydZkKa5GHgJVBaCpbayFdIQIWrWIOQMxERp6oQZs/edit?usp=drive_link"},
       { nombre: "Reporte de Comunidades", link: "https://docs.google.com/spreadsheets/d/1r4TgzVZ3jpzAk5mR5e9fxkCdwJfH6Bb6tvpTJFbOdWs/edit?usp=drive_link"},
       { nombre: "Proyeccion Comunidades", link: "https://docs.google.com/spreadsheets/d/18w8kcVfaZJKu_KWN-DFjGp-feLQ-sePfv60V5ZMD0_M/edit?usp=drive_link"},
-      {nombre: "BACDJ-TS001-A Comunicado comunidades", link: "file:///C:/Users/josel/AppData/Local/Temp/cac8d3c1-4e3a-4446-9ba7-3f35000fd147_DOCUMENTOS_EDITADOS_PDF[1].zip.147/BACDJ-TS001-A%20%20Comunicado%20comunidades.docx.pdf" },
-      {nombre: "BACDJ-TS003-A Recepción comunidad No. cliente", link: "file:///C:/Users/josel/AppData/Local/Temp/ee11ebf7-cb21-44cd-9f63-8f93dffacf19_DOCUMENTOS_EDITADOS_PDF[1].zip.f19/BACDJ-TS003-A%20%20Recepci%C3%B3n%20comunidad%20No.%20cliente.docx.pdf" },
+      {nombre: "BACDJ-TS001-A Comunicado comunidades", link: "https://drive.google.com/file/d/11dBE2I44LyfbPEguiwBaq_KtFOOq3aLF/view?usp=drive_link" },
+      {nombre: "BACDJ-TS003-A Recepción comunidad No. cliente", link: "https://drive.google.com/file/d/11bq9ZYVlnl3nRGT062KNPdeAO-vRCRn1/view?usp=drive_link" },
 
     ],
 
     formatos: [
-      {nombre: "BACDJ-TS001-A Comunicado comunidades", link: "file:///C:/Users/josel/AppData/Local/Temp/cac8d3c1-4e3a-4446-9ba7-3f35000fd147_DOCUMENTOS_EDITADOS_PDF[1].zip.147/BACDJ-TS001-A%20%20Comunicado%20comunidades.docx.pdf" },
-      {nombre: "BACDJ-TS002-A Producto a granel", link: "file:///C:/Users/josel/AppData/Local/Temp/bf778271-fe04-45b6-95b7-2b5727fbef07_DOCUMENTOS_EDITADOS_PDF[1].zip.f07/BACDJ-TS002-A%20%20Producto%20a%20granel.docx.pdf" },
-      {nombre: "BACDJ-TS003-A Recepción comunidad No. cliente", link: "file:///C:/Users/josel/AppData/Local/Temp/ee11ebf7-cb21-44cd-9f63-8f93dffacf19_DOCUMENTOS_EDITADOS_PDF[1].zip.f19/BACDJ-TS003-A%20%20Recepci%C3%B3n%20comunidad%20No.%20cliente.docx.pdf" },
-      {nombre: "BACDJ-TS004-A Minuta", link: "file:///C:/Users/josel/AppData/Local/Temp/46cdd1f6-ca1b-4236-b4ee-60f98fe29fb1_DOCUMENTOS_EDITADOS_PDF[1].zip.fb1/BACDJ-TS004-A%20%20Minuta.docx.pdf" },
-      {nombre: "BACDJ-TS005-A Recepción directos", link: "file:///C:/Users/josel/AppData/Local/Temp/47e83c85-03c1-4909-abee-e38b9d71a4e2_DOCUMENTOS_EDITADOS_PDF[1].zip.4e2/BACDJ-TS005-A%20%20Recepci%C3%B3n%20directos.docx.pdf" },
-      {nombre: "BACDJ-TS006-A Reglamento y carta compromiso 2025", link: "file:///C:/Users/josel/AppData/Local/Temp/b734f82c-4a4b-4413-a3df-50c289fadd19_DOCUMENTOS_EDITADOS_PDF[1].zip.d19/BACDJ-TS006-A%20%20Reglamento%20y%20carta%20compromiso%202025.docx.pdf" },
-      {nombre: "BACDJ-TS007-A Supervision por llamada", link: "file:///C:/Users/josel/AppData/Local/Temp/13bf6694-14f4-4736-87f1-f10eb83154ac_DOCUMENTOS_EDITADOS_PDF[1].zip.4ac/BACDJ-TS007-A%20%20Supervision%20por%20llamada.docx.pdf" },
-      {nombre: "BACDJ-TS008-A Uso de imagen", link: "file:///C:/Users/josel/AppData/Local/Temp/f5c0ca29-4aa6-4d63-ba5d-0e06bab2098f_DOCUMENTOS_EDITADOS_PDF[1].zip.98f/BACDJ-TS008-A%20%20Uso%20de%20imagen.docx.pdf" },
-      {nombre: "BACDJ-TS009-A Formato Socionutricio", link: "file:///C:/Users/josel/AppData/Local/Temp/406b2766-7bee-4bee-92d4-8cd339c925c8_DOCUMENTOS_EDITADOS_PDF[1].zip.5c8/BACDJ-TS009-A%20%20Formato%20Socionutricio.docx.pdf" },
-      {nombre: "BACDJ-TS010-A Formato_Institucion 2022", link: "file:///C:/Users/josel/AppData/Local/Temp/5aa7ee50-5467-42ff-ad64-39b147b5e21d_DOCUMENTOS_EDITADOS_PDF[1].zip.21d/BACDJ-TS010-A%20%20Formato_Institucion%202022.docx.pdf" },
+      {nombre: "BACDJ-TS001-A Comunicado comunidades", link: "https://drive.google.com/file/d/11dBE2I44LyfbPEguiwBaq_KtFOOq3aLF/view?usp=drive_link" },
+      {nombre: "BACDJ-TS002-A Producto a granel", link: "https://drive.google.com/file/d/1ay0_XihQ6fR-vLOt08xvd1hb1H9hdg8y/view?usp=drive_link" },
+      {nombre: "BACDJ-TS003-A Recepción comunidad No. cliente", link: "https://drive.google.com/file/d/11bq9ZYVlnl3nRGT062KNPdeAO-vRCRn1/view?usp=drive_link" },
+      {nombre: "BACDJ-TS004-A Minuta", link: "https://drive.google.com/file/d/1t2SxF7FbBblR17xnN3BhPH7y11qirKOV/view?usp=drive_link" },
+      {nombre: "BACDJ-TS005-A Recepción directos", link: "https://drive.google.com/file/d/10xkc_a1QHPnXZyZ0hbcVqWYoib-VpW0N/view?usp=drive_link" },
+      {nombre: "BACDJ-TS006-A Reglamento y carta compromiso 2025", link: "https://drive.google.com/file/d/1nLNxXqW2Y3d3_-QT89vQApL-TD-vHNBu/view?usp=drive_link" },
+      {nombre: "BACDJ-TS007-A Supervision por llamada", link: "https://drive.google.com/file/d/106IObH5FJGZ4YzxtV_e8Vf4HGm6s3w7_/view?usp=drive_link" },
+      {nombre: "BACDJ-TS008-A Uso de imagen", link: "https://drive.google.com/file/d/1FBKvXSm6jABh5r6vKfVKAkwvVOTFiTXa/view?usp=drive_link" },
+      {nombre: "BACDJ-TS009-A Formato Socionutricio", link: "https://drive.google.com/file/d/1DBH1Ji6eX0lnXo_LkNJDw_lclhpJHBUB/view?usp=drive_link" },
+      {nombre: "BACDJ-TS010-A Formato_Institucion 2022", link: "https://drive.google.com/file/d/1GJinGOjyit970kF3DTuiOzDBm2Vbj3b6/view?usp=drive_link" },
     ]
   };
   
